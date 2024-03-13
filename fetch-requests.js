91033d45c4a2902d862205e6f19cfcd8e5e56957
AnthonyBotha <anthony.botha@outlook.com>
2024-03-12 21:35:41 -0700

    Basic Complete

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,22 +5,22 @@ Paste your code for fetch requests here once you finish each task.
 
 /* =============== 1. Print the status code of the response =============== */
 
-// Your code here 
+fetch("/products").then(response => response.status);
 
 
 
 /* ====== 2. Print true if the status of the response was successful ====== */
 
-// Your code here 
+fetch("/products").then(response => response.ok);
 
 
 
 /* =================== 3. Print the Content-Type Header =================== */
 
-// Your code here 
+fetch("/products").then(response => response.headers.get("Content-Type"));
 
 
 
 /* ============== 4. Print the body of the response as text =============== */
 
-// Your code here 
+fetch("/products").then(response => response.text());
